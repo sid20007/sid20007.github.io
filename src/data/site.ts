5a57e9f471b80bed9edcf06f14ddafdd0b761a6e
sid20007 <sid@example.com>
2026-05-11 13:08:48 +0000

    chore: update broken linkedin link

--- src/data/site.ts
+++ src/data/site.ts
@@ -6,7 +6,7 @@ export const siteConfig = {
   url: "https://sid20007.me",
   links: {
     github: "https://github.com/sid20007",
-    linkedin: "https://linkedin.com/in/sid20007",
+    linkedin: "https://www.linkedin.com/in/siddhartha-g-9761643a8/",
     email: "workwsid1@gmail.com",
   },
 };
